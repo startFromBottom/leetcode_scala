054e0973934ae50353cec38464bbc097021335f0
startFromBottom <uhh0701@gmail.com>
2020-08-11 21:55:29 +0900

    add 94_Binary_Tree_Inorder_Traversal.scala

---
+++ 94_Binary_Tree_Inorder_Traversal.scala
@@ -0,0 +1,57 @@
+/*
+
+problem link : https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+ */
+
+
+// Definition for a binary tree node.
+class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+  var value: Int = _value
+  var left: TreeNode = _left
+  var right: TreeNode = _right
+}
+
+object Solution {
+  // recursion solution
+  def inorderTraversal(root: TreeNode): List[Int] = {
+
+    val res = new collection.mutable.ListBuffer[Int]()
+
+    def go(root: TreeNode): Unit = {
+      if (root != null) {
+        go(root.left)
+        res += root.value
+        go(root.right)
+      }
+    }
+
+    go(root)
+    res.toList
+
+  }
+}
+
+object Solution2 {
+
+  def inorderTraversal(root: TreeNode): List[Int] = {
+    // iteration solution
+    var cur = root
+    var stack: List[TreeNode] = List()
+    var result: List[Int] = List()
+
+    while (cur != null || stack.nonEmpty) {
+      while (cur != null) {
+        stack = cur :: stack
+        cur = cur.left
+      }
+      cur = stack.head
+      result = cur.value :: result
+      stack = stack.tail
+      cur = cur.right
+    }
+
+    result
+  }
+
+}
